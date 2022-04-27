334e0156f95fb21f9d24b2c2907b583a2444d96a
Mitchell Goering <mitchell.goering@gmail.com>
2022-04-27 14:44:20 -0700

    initial commit
    Co-authored-by: Tom <tomsharratt@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [guessCounter, setGuessCounter] = useState(0);
+  const [guess, setGuess] = useState("");
+  const [victory, setVictory] = useState(false);
+
+  const [response, setResponse] = useState([
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]],
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]],
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]],
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]],
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]],
+    [[' ', 3], [' ', 3], [' ', 3], [' ', 3], [' ', 3]]
+  ])
+
+  const checkVictory = () => {
+    const winner = response[guessCounter].every(item => item[1] === 2)
+    if (winner === true) {
+      setVictory(true)
+    }
+  }
+
+  const handleChange = (event) => {
+    setGuess(event.target.value)
+  }
+
+  const checkWord = (event) => {
+    event.preventDefault()
+
+    if (canGuess()) {
+      return;
+    }
+
+    const requestOptions = {
+      method:"POST", 
+      headers: {"content-type": "application/json"},
+      body: JSON.stringify({guess:guess})
+    }
+    fetch("http://localhost:5000/check", requestOptions)
+      .then((response) => response.json())
+      .then(displayGuess)
+      .then(incrementGuessCounter)
+      .then(checkVictory)
+      .catch((err) => console.error(err))
+  }
+
+  const displayGuess = (guess) => {
+    response[guessCounter] = guess
+    setResponse(response)
+  }
+
+  const incrementGuessCounter = () => {
+    setGuessCounter(guessCounter + 1)
+  }
+
+  // DRY (Don't repeat yourself) and it is the D in SOLID
+  const canGuess = () => {
+    return guessCounter >= 6
+  }
+
+  let message;
+  if (victory === true) {
+    message = <div><h1> congrats! </h1></div>;
+  } else if (victory === false && canGuess()) {
+    message = <div><h2> try again tomorrow! </h2></div>;
+  } else {
+    message = <h3> guess a word! </h3>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      {message}
+    </div>    
+    <form onSubmit={checkWord}>
+      <label>
+          Guess: 
+          <input type="text" value={guess} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    <div className="wordle-grid">
+      {
+        response.map(guess => {
+          return guess.map(letter => {
+            if (letter[0] === null) {
+              return (<div className="letter color-blank"></div>)
+            } else {
+              return (
+                <div className={`letter color-${letter[1]}`}>{letter[0]}</div>
+              )
+            }
+          })
+        })
+      }
     </div>
-  );
-}
+  </div>
+  );}
 
 export default App;
